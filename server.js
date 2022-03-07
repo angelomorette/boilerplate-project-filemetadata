5e6351125bc0b84e6ba297a6f2e0586bd193c41d
angelomorette <angelomorette@gmail.com>
2022-03-06 22:04:00 -0300

    upload de arquivos usando middleware Multer

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+//mimiddleware for handling multipart/form-data
+const multer = require('multer');
+const upload = multer({dest: process.cwd() + '/uploads'});
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +15,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
